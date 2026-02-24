9068a6578a4d2c5c49be2ff2b9a05bc59fadf8b0
thanhphucnguyen1810 <thanhphuc1810.work@gmail.com>
2026-02-24 15:30:49 +0700

    feat: add axios interceptor

--- web-base-jwt-axios-interceptors/src/pages/Login.jsx
+++ web-base-jwt-axios-interceptors/src/pages/Login.jsx
@@ -8,8 +8,8 @@ import Zoom from '@mui/material/Zoom'
 import Alert from '@mui/material/Alert'
 import { useForm } from 'react-hook-form'
 import Typography from '@mui/material/Typography'
-import TrungQuanDevIcon from '../assets/trungquandev-logo.png'
-import axios from 'axios'
+import DevIcon from '../assets/trungquandev-logo.png'
+import authorizedAxiosInstance from '~/utils/authorizedAxios'
 import { toast } from 'react-toastify'
 import { API_ROOT } from '~/utils/constants'
 
@@ -18,13 +18,9 @@ function Login() {
 
   const submitLogIn = async (data) => {
     console.log('submit login: ', data)
-    try {
-      const res = await axios.post(`${API_ROOT}/v1/users/login`, data)
-      console.log(res.data)
-      toast.success(res.data?.message)
-    } catch (error) {
-      toast.error(error.response?.data?.message || error?.message)
-    }
+    const res = await authorizedAxiosInstance.post(`${API_ROOT}/v1/users/login`, data)
+    console.log(res.data)
+    toast.success(res.data?.message)
   }
 
   return (
@@ -44,7 +40,7 @@ function Login() {
         <Zoom in={true} style={{ transitionDelay: '200ms' }}>
           <MuiCard sx={{ minWidth: 380, maxWidth: 380, marginTop: '6em', p: '0.5em 0', borderRadius: 2 }}>
             <Box sx={{ width: '70px', bgcolor: 'white', margin: '0 auto' }}>
-              <img src={TrungQuanDevIcon} alt='trungquandev' width='100%' />
+              <img src={DevIcon} alt='trungquandev' width='100%' />
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'center', color: theme => theme.palette.grey[500] }}>
               <Box>
--- web-base-jwt-axios-interceptors/src/utils/authorizedAxios.js
+++ web-base-jwt-axios-interceptors/src/utils/authorizedAxios.js
@@ -1,1 +1,37 @@
 // Author: thanhphuc1810
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+let authorizedAxiosInstance = axios.create()
+
+// thời gian chờ tối đa của một request: để 10 phút
+authorizedAxiosInstance.defaults.timeout = 1000 * 60 * 10
+
+//withCredentials: cho phép axios đính kèm và gửi cookie trong mỗi request lên BE
+// (trong TH sử dụng JWT tokens theo cơ chế httpOnly cookie)
+// authorizedAxiosInstance.defaults.withCredentials = true
+
+// CẤU HÌNH Interceptors (Bộ đánh chặn vào giữa mọi res và req)
+
+// add request interceptor: can thiệp vào giữa những cái req API
+authorizedAxiosInstance.interceptors.request.use((config) => {
+  // Do something before request is sent
+  return config
+}, (error) => {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// add response interceptor: can thiệp vào giữa những res từ API
+authorizedAxiosInstance.interceptors.response.use((response) => {
+  // Do something with response data
+  return response
+}, (error) => {
+  // Do something with response error
+  if (error.response.status !== 410) {
+    toast.error(error.response?.data?.message || error?.message)
+  }
+  return Promise.reject(error)
+})
+
+export default authorizedAxiosInstance
